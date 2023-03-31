db1d3e815da906e9d54523b69bfa7c05ba123fb1
Georgi.Olentsenko <georgi.olentsenko@gmail.com>
2023-03-31 17:42:40 +0200

    Added column assignment testing

---
+++ tests/test_receiver.py
@@ -0,0 +1,58 @@
+
+
+import unittest
+from waps_ies import tcpreceiver, interface
+import time
+
+class TestReceiver(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+
+        self.receiver = tcpreceiver.TCP_Receiver("192.168.0.1", 12345, "output/")
+        self.interface = interface.WAPS_interface(self.receiver)
+        self.receiver.add_interface(self.interface)
+        while (not self.receiver.interface.window_open):
+            time.sleep(0.1)
+
+    def test_gui_column_assignment(self):
+        """ A receiver and test GUI column assignment """
+
+        receiver=self.receiver
+
+        ec_addr1 = 172
+        ec_addr2 = 173
+        ec_addr3 = 174
+        ec_addr4 = 175
+        ec_addr5 = 176
+
+        receiver.get_ecs_state_index(ec_addr1)
+        receiver.get_ecs_state_index(ec_addr2)
+
+        receiver.assign_ec_column(ec_addr1)
+        self.assertEqual(receiver.ECs_state[receiver.get_ecs_state_index(ec_addr1)]["gui_column"], 0)
+        receiver.assign_ec_column(ec_addr5)
+        self.assertEqual(receiver.ECs_state[receiver.get_ecs_state_index(ec_addr5)]["gui_column"], 1)
+        receiver.assign_ec_column(ec_addr2)
+        self.assertEqual(receiver.ECs_state[receiver.get_ecs_state_index(ec_addr2)]["gui_column"], 2)
+        receiver.assign_ec_column(ec_addr3)
+        self.assertEqual(receiver.ECs_state[receiver.get_ecs_state_index(ec_addr3)]["gui_column"], 3)
+        receiver.assign_ec_column(ec_addr4)
+        self.assertEqual(receiver.ECs_state[receiver.get_ecs_state_index(ec_addr4)]["gui_column"], None)
+        receiver.assign_ec_column(ec_addr1)
+        self.assertEqual(receiver.ECs_state[receiver.get_ecs_state_index(ec_addr1)]["gui_column"], 0)
+        receiver.assign_ec_column(ec_addr5)
+        self.assertEqual(receiver.ECs_state[receiver.get_ecs_state_index(ec_addr5)]["gui_column"], 1)
+        receiver.assign_ec_column(ec_addr2)
+        self.assertEqual(receiver.ECs_state[receiver.get_ecs_state_index(ec_addr2)]["gui_column"], 2)
+        receiver.assign_ec_column(ec_addr3)
+        self.assertEqual(receiver.ECs_state[receiver.get_ecs_state_index(ec_addr3)]["gui_column"], 3)
+        receiver.assign_ec_column(ec_addr4)
+        self.assertEqual(receiver.ECs_state[receiver.get_ecs_state_index(ec_addr4)]["gui_column"], None)
+
+    @classmethod
+    def tearDownClass(self):
+        self.receiver.interface.close()
+
+if __name__ == '__main__':
+    unittest.main()
