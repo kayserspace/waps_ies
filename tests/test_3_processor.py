be4538e7036d5994c36bbfd381de3a15adc5dca7
Georgi.Olentsenko <georgi.olentsenko@gmail.com>
2023-05-09 10:57:10 +0100

    Added test of missing pcakets based on test bed files. Added ignore for test files and build

--- tests/test_3_processor.py
+++ tests/test_3_processor.py
@@ -172,16 +172,129 @@ class TestProcessor(unittest.TestCase):
 
         self.assertEqual(len(packet_list), 309)
 
-        packet_list.pop(17)
-        packet_list.pop(125)
+        flir_packet = packet_list.pop(20)
+        jpg_packet = packet_list.pop(125)
 
         incomplete_images = []
         self.receiver.incomplete_images = waps_ies.processor.sort_biolab_packets(packet_list, incomplete_images, self.receiver)
         self.assertEqual(len(self.receiver.incomplete_images), 2)
+
+        save_file_dir = "tests/output/" + incomplete_images[0].ccsds_time.strftime('%Y%m%d') + '/'
+        if os.path.exists(save_file_dir):
+            shutil.rmtree(save_file_dir)
+
         self.receiver.incomplete_images = waps_ies.processor.save_images(self.receiver.incomplete_images,
                                                                          'tests/output/',
                                                                          self.receiver)
-        self.assertEqual(len(self.receiver.incomplete_images), 2)
+        # Check that correct missing packet is listed
+        self.assertEqual(self.receiver.incomplete_images[0].get_missing_packets(), [5])
+        self.assertEqual(self.receiver.incomplete_images[1].get_missing_packets(), [2])
+
+        # Compare original to the new JPEG files
+        original_file_data = None
+        new_file_data = None
+        with open("tests/test_bed_files/with_missing_packets/EC_171_uCAM_095400_m1_2054_99.jpg", 'rb') as file:
+            original_file_data = file.read()
+            file.close()
+        with open(self.receiver.incomplete_images[1].latest_saved_file, 'rb') as file:
+            new_file_data = file.read()
+            file.close()
+
+        # JPEGs are equal except for last packet is not cut as it should be
+        # according to specification (12 bytes extra)
+        self.assertEqual(new_file_data, original_file_data[:len(new_file_data)])
+        self.assertEqual(len(original_file_data) - len(new_file_data), 0)
+
+        
+        # Compare original to the new raw FLIR files
+        with open("tests/test_bed_files/with_missing_packets/EC_171_FLIR_095400_m0_885_98_data.csv", 'rb') as file:
+            original_file_data = file.read()
+            file.close()
+        with open(self.receiver.incomplete_images[0].latest_saved_file_data, 'rb') as file:
+            new_file_data = file.read()
+            file.close()
+
+        self.assertEqual(new_file_data, original_file_data[:len(new_file_data)])
+
+        # Compare original to the new meta FLIR files
+        with open("tests/test_bed_files/with_missing_packets/EC_171_FLIR_095400_m0_885_98_tm.txt", 'rb') as file:
+            original_file_data = file.read()
+            file.close()
+        with open(self.receiver.incomplete_images[0].latest_saved_file_tm, 'rb') as file:
+            new_file_data = file.read()
+            file.close()
+
+        self.assertEqual(new_file_data, original_file_data[:len(new_file_data)])
+
+        # Compare original to the new meta FLIR files
+        with open("tests/test_bed_files/with_missing_packets/EC_171_FLIR_095400_m0_885_98.bmp", 'rb') as file:
+            original_file_data = file.read()
+            file.close()
+        with open(self.receiver.incomplete_images[0].latest_saved_file, 'rb') as file:
+            new_file_data = file.read()
+            file.close()
+
+        flir_image_name = self.receiver.incomplete_images[0].image_name
+        ucam_image_name = self.receiver.incomplete_images[1].image_name
+        output_dir = "tests/output/" + datetime.datetime.now().strftime("%Y%m%d") + '/'
+
+        # Reintegrate FLIR packet
+        self.receiver.incomplete_images = waps_ies.processor.sort_biolab_packets([flir_packet], incomplete_images, self.receiver)
+        self.receiver.incomplete_images = waps_ies.processor.save_images(self.receiver.incomplete_images,
+                                                                         'tests/output/',
+                                                                         self.receiver)
+        self.assertEqual(len(self.receiver.incomplete_images), 1)
+
+        # Compare original to the new raw FLIR files
+        with open("tests/test_bed_files/ir_20221024_1049_pic.csv", 'rb') as file:
+            original_file_data = file.read()
+            file.close()
+        with open(output_dir+flir_image_name+"_100_data.csv", 'rb') as file:
+            new_file_data = file.read()
+            file.close()
+
+        self.assertEqual(new_file_data, original_file_data[:len(new_file_data)])
+
+        # Compare original to the new meta FLIR files
+        with open("tests/test_bed_files/ir_20221024_1049_tm.txt", 'rb') as file:
+            original_file_data = file.read()
+            file.close()
+        with open(output_dir+flir_image_name+"_100_tm.txt", 'rb') as file:
+            new_file_data = file.read()
+            file.close()
+
+        self.assertEqual(new_file_data, original_file_data[:len(new_file_data)])
+
+        # Compare original to the new meta FLIR files
+        with open("tests/test_bed_files/ir_20221024_1049.bmp", 'rb') as file:
+            original_file_data = file.read()
+            file.close()
+        with open(output_dir+flir_image_name+"_100.bmp", 'rb') as file:
+            new_file_data = file.read()
+            file.close()
+
+        self.assertEqual(new_file_data, original_file_data[:len(new_file_data)])
+
+        # Reintegrate uCAM packet
+        self.receiver.incomplete_images = waps_ies.processor.sort_biolab_packets([jpg_packet], incomplete_images, self.receiver)
+        self.receiver.incomplete_images = waps_ies.processor.save_images(self.receiver.incomplete_images,
+                                                                         'tests/output/',
+                                                                         self.receiver)
+        self.assertEqual(len(self.receiver.incomplete_images), 0)
+
+        # Compare original to the new JPEG files
+        original_file_data = None
+        new_file_data = None
+        with open("tests/test_bed_files/color_20221024_1052.jpeg", 'rb') as file:
+            original_file_data = file.read()
+            file.close()
+        with open(output_dir+ucam_image_name+"_100.jpg", 'rb') as file:
+            new_file_data = file.read()
+            file.close()
+
+        # JPEGs are equal except for last packet is not cut as it should be
+        # according to specification (12 bytes extra)
+        self.assertEqual(new_file_data, original_file_data[:len(new_file_data)])
 
 if __name__ == '__main__':
     unittest.main()
